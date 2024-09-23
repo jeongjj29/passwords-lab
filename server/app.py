24cb6017f855b6676d82b8fc9e875adf5e5c2cc8
Jae Jeong <jeongjj29@gmail.com>
2024-09-23 14:15:26 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -6,38 +6,59 @@ from flask_restful import Resource
 from config import app, db, api
 from models import User
 
+
 class ClearSession(Resource):
 
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
+
+        session["page_views"] = None
+        session["user_id"] = None
 
         return {}, 204
 
+
 class Signup(Resource):
-    
+
     def post(self):
         json = request.get_json()
-        user = User(
-            username=json['username']
-        )
-        user.password_hash = json['password']
+        user = User(username=json["username"])
+        user.password_hash = json["password"]
         db.session.add(user)
         db.session.commit()
         return user.to_dict(), 201
 
+
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session.get("user_id"):
+            user = User.query.filter_by(id=session.get("user_id")).first()
+            return user.to_dict(), 200
+        return {}, 204
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        username = request.get_json()["username"]
+        user = User.query.filter_by(username=username).first()
+
+        password = request.get_json()["password"]
+        if user and user.authenticate(password):
+            session["user_id"] = user.id
+            return user.to_dict(), 200
+        return {"error": "Invalid username or password"}, 401
+
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session["user_id"] = None
+        return {}, 204
 
-api.add_resource(ClearSession, '/clear', endpoint='clear')
-api.add_resource(Signup, '/signup', endpoint='signup')
 
-if __name__ == '__main__':
+api.add_resource(ClearSession, "/clear", endpoint="clear")
+api.add_resource(Signup, "/signup", endpoint="signup")
+api.add_resource(CheckSession, "/check_session", endpoint="check_session")
+api.add_resource(Login, "/login", endpoint="login")
+api.add_resource(Logout, "/logout", endpoint="logout")
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
